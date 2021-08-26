81da663962ffa8a19702b769f8326a5a2129d132
Thabo Setsubi <thabosetsubi3@gmail.com>
2021-08-26 19:23:25 +0200

    Update

--- Scripts/appointment.js
+++ Scripts/appointment.js
@@ -1,1 +1,15 @@
 const baseURL = "https://desolate-meadow-13744.herokuapp.com/view-appointment/";
+const container = document.querySelector("#timetable");
+
+fetch(baseURL)
+	.then((res) => res.json())
+	.then((data) => {
+		console.log(data);
+		let times = data.data;
+		container.innerHTML = "";
+		times.forEach((time) => {
+			container.innerHTML += `
+            
+            `;
+		});
+	});

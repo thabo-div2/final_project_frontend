4acef7bf7157fab0ec4fbd074acece7133cbaf64
Thabo Setsubi <thabosetsubi3@gmail.com>
2021-08-26 15:42:23 +0200

    Update

---
+++ Scripts/patient.js
@@ -0,0 +1,24 @@
+const baseURL = "https://desolate-meadow-13744.herokuapp.com/view-patient/";
+const conatiner = document.querySelector("#view-patients");
+
+fetch(baseURL)
+	.then((res) => res.json())
+	.then((data) => {
+		console.log(data);
+		let patients = data.data;
+		conatiner.innerHTML = "";
+		patients.forEach((patient) => {
+			conatiner.innerHTML += `
+            <div class="patient">
+                <h3>${patient.first_name} ${patient.last_name}</h3>
+                <div class="details">
+                    <p>Address: ${patient.address}</p>
+                    <p>Email: ${patient.email}</p>
+                    <p>Gender: ${patient.gender}</p>
+                    <p>Identification Number: ${patient.id_num}</p>
+                    <p>Cell Number: +27 ${patient.phone_num}</p>
+                </div>
+            </div>
+            `;
+		});
+	});

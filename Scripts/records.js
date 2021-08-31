e1559ebc8f66fe70bd33c4db91f330e11a445d3b
Thabo Setsubi <thabosetsubi3@gmail.com>
2021-08-31 11:15:55 +0200

    Update

--- Scripts/records.js
+++ Scripts/records.js
@@ -1,1 +1,19 @@
 const baseURL = "https://desolate-meadow-13744.herokuapp.com/view-illness/";
+let container = document.querySelector("#records");
+fetch(baseURL)
+	.then((res) => res.json())
+	.then((data) => {
+		console.log(data);
+		container.innerHTML = "";
+		let records = data.data;
+		records.forEach((record) => {
+			container.innerHTML += `
+            <div class="records-details">
+                <h3>Patient ID: ${record.patient_id}</h3>
+                <p>Name: ${record.name}</p>
+                <p>Type: ${record.type}</p>
+                <p>Description: ${record.description}</p>
+            </div>
+            `;
+		});
+	});
